bd2c23c7ef3b2245aa51676afae97e8a65379f7b
arunstiwari <arunstiwari@gmail.com>
2021-07-05 16:47:39 +0530

    Created a base project to work with authentication service

--- authentication/src/main/java/com/tekmentors/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/tekmentors/authentication/AuthenticationApplication.java
@@ -1,13 +1,13 @@
-package com.tekmentor.authenticationservice;
+package com.tekmentors.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationServiceApplication {
+public class AuthenticationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthenticationServiceApplication.class, args);
+        SpringApplication.run(AuthenticationApplication.class, args);
     }
 
 }
--- authentication/src/test/java/com/tekmentors/authentication/AuthenticationApplicationTests.java
+++ authentication/src/test/java/com/tekmentors/authentication/AuthenticationApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tekmentor.authenticationservice;
+package com.tekmentors.authentication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthenticationServiceApplicationTests {
+class AuthenticationApplicationTests {
 
     @Test
     void contextLoads() {
