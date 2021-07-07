87b9ff7a6cac34ea1cbb48d3114a566656849272
arunstiwari <arunstiwari@gmail.com>
2021-07-07 11:19:30 +0530

    Provided an implementation for Ecommerce app

--- productservice/src/main/java/com/tekmentor/productservice/controller/ProductController.java
+++ productservice/src/main/java/com/tekmentor/productservice/controller/ProductController.java
@@ -1,15 +1,29 @@
 package com.tekmentor.productservice.controller;
 
+import com.tekmentor.productservice.config.ProductConfig;
+import com.tekmentor.productservice.model.Customer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 public class ProductController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ProductConfig config;
 
     @GetMapping("/products")
     public String showProducts(){
+        List<Customer> customers = restTemplate.getForObject(config.getCustomerservice() + "/customers",
+                List.class);
+        log.info(" customers : {}",customers);
         return "Products";
     }
 }
---
+++ productservice/src/main/java/com/tekmentor/productservice/model/Customer.java
@@ -0,0 +1,8 @@
+package com.tekmentor.productservice.model;
+
+public class Customer {
+
+    private Long id;
+    private String name;
+    private String email;
+}
