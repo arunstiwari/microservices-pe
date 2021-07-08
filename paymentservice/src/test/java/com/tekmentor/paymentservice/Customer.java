3273607b5efffbe9882699d4a21d24b526d67db8
arunstiwari <arunstiwari@gmail.com>
2021-07-08 14:14:28 +0530

    Added the test

--- paymentservice/src/test/java/com/tekmentor/paymentservice/Customer.java
+++ paymentservice/src/test/java/com/tekmentor/paymentservice/Customer.java
@@ -1,2 +1,12 @@
-package com.tekmentor.paymentservice;public class Customer {
+package com.tekmentor.paymentservice;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Customer {
+    private Long id;
+
+    private String name;
 }
--- paymentservice/src/test/java/com/tekmentor/paymentservice/PaymentserviceApplicationTests.java
+++ paymentservice/src/test/java/com/tekmentor/paymentservice/PaymentserviceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.tekmentor.paymentservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.junit.StubRunnerExtension;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PaymentserviceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+   @RegisterExtension
+    public StubRunnerExtension stubRunner = new StubRunnerExtension()
+           .downloadStub("com.tekmentor","customersvc","0.0.1-SNAPSHOT", "stubs")
+           .withPort(9098)
+           .stubsMode(StubRunnerProperties.StubsMode.LOCAL);
+
+   @Test
+    public void shouldReturnCustomerForAnIdwithValue1(){
+       RestTemplate restTemplate = new RestTemplate();
+       ResponseEntity<Customer> customerResponseEntity = restTemplate.getForEntity("http://localhost:9098/customers/1", Customer.class);
+       assertEquals("1", customerResponseEntity.getBody().getId());
+       assertEquals("Customer-1", customerResponseEntity.getBody().getName());
+
+   }
 
 }
