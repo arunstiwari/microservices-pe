8139e4d34daba61f9ea34d4970e13d847c2e802e
arunstiwari <arunstiwari@gmail.com>
2021-07-08 15:05:36 +0530

    Added the basic kafka consumer

---
+++ shippingservice/src/main/java/com/tekmentor/shippingservice/consumer/KafkaConsumer.java
@@ -0,0 +1,27 @@
+package com.tekmentor.shippingservice.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "shipping-topic")
+    public void consumeAnyShippingEvents(String message) throws InterruptedException {
+        log.info("New message in shipping topic : {}",message);
+
+        Thread.sleep(1000);
+        log.info("Prepairing for dispatch");
+
+
+        Thread.sleep(1000);
+        log.info("Shipped the good");
+
+
+        Thread.sleep(1000);
+        log.info("Goods have been delivered");
+    }
+
+}
